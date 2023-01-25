794b26393c61e7e1515d5a9759dd9a7c51ad245b
majegoid <majegoid@gmail.com>
2023-01-24 18:09:02 -0600

    changes indicator highlight color to active bg gray

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { createImageSlider } from './createImageSlider';
 import img2 from './images/wallhaven-39vzm3.jpg';
 import img1 from './images/wallhaven-8xz1mk.jpg';
-// import img3 from './images/wallhaven-eywz3k.jpg';
-// import img4 from './images/wallhaven-x8xjel.jpg';
+import img3 from './images/wallhaven-eywz3k.png';
+import img4 from './images/wallhaven-x8xjel.png';
 import img5 from './images/wallhaven-ym681g.jpg';
 import './styles/index.css';
 
@@ -11,8 +11,8 @@ const imageSlider = createImageSlider(
   [
     { url: img1, description: 'Image' },
     { url: img2, description: 'Image' },
-    // { url: img3, description: 'Image' },
-    // { url: img4, description: 'Image' },
+    { url: img3, description: 'Image' },
+    { url: img4, description: 'Image' },
     { url: img5, description: 'Image' },
   ],
   5000,
